d569a8202c6eda9d7c876ef7bcdb28be748ab12d
vitorcx <vitorcx010@gmail.com>
2020-09-24 23:08:02 -0300

    Add geolocator to get user position

--- lib/managers/user_manager.dart
+++ lib/managers/user_manager.dart
@@ -1,14 +1,25 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:zefiro_app/models/user.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class UserManager extends ChangeNotifier {
   User user;
+  Future<SharedPreferences> _sharedPreferences =
+      SharedPreferences.getInstance();
 
   UserManager() {
+    print("UserManager Instanciado");
     _loadCurrentUser();
   }
 
-  Future<void> _loadCurrentUser() {
-    print(user.toString());
+  Future<void> _loadCurrentUser() async {
+    final userData = await _sharedPreferences;
+    if (userData.getString('position') == null) {
+      user = User();
+      await user.getPosition();
+    }
+    notifyListeners();
   }
 }
--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,9 +1,17 @@
+import 'dart:ffi';
+
 import 'package:geolocator/geolocator.dart';
 
 class User {
-  User({this.age, this.position});
+  User({this.age, this.position, this.localization});
 
   int age;
   Position position;
   String localization;
+
+  Future<void> getPosition() async {
+    Position position =
+        await getCurrentPosition(desiredAccuracy: LocationAccuracy.medium);
+    this.position = position;
+  }
 }
